d40995f2cdf5b1ca13a2296e758a6c0db1521cd5
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-10-07 14:56:38 -0400

    added bishop,rook,knight classes

---
+++ ChessLib/Bishop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLib
+{
+    public class Bishop : Piece
+    {
+        public override bool CanMove(Board board, Square start, Square end)
+        {
+            if (Math.Abs(start.getX() - end.getX()) == Math.Abs(start.getY() - end.getY()))
+            {
+                // insert check that every square between start and end is empty
+                return true;
+            }
+            else return false;
+
+        }
+    }
+}
---
+++ ChessLib/Knight.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLib
+{
+    class Knight : Piece
+    {
+        public override bool CanMove(Board board, Square start, Square end)
+        {
+            // we can't move the piece to a spot that has
+            // a piece of the same colour
+            if (end.getPiece().isWhite() == this.isWhite())
+            {
+                return false;
+            }
+
+            int x = Math.Abs(start.getX() - end.getX());
+            int y = Math.Abs(start.getY() - end.getY());
+            return x * y == 2;
+        }
+    }
+}
