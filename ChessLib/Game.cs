145787acbf142aa39dc6ac9702f8f1921b374b49
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-10-15 16:00:31 -0400

    game class

---
+++ ChessLib/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLib
+{
+    public class Game
+    {
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+        public bool Win { get; set; }
+
+
+        // Move()
+
+
+        //Capture()
+
+
+    }
+}
